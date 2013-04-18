649ba14fde9fa8838f94c689cb2227c03d52cae8
hasit <hasit@lavabit.com>
2013-04-18 18:39:03 +0530

    time in binary format

---
+++ binclock.cpp
@@ -0,0 +1,53 @@
+#include<time.h>
+#include<unistd.h>
+#include<iostream>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+string converttobin(int);
+
+int main()
+{
+  time_t now;
+  int seconds, minutes, hours;
+  int binseconds, binminutes, binhours;
+  struct tm* mytime;
+
+  while(1)
+    {
+      now = time(0);
+      mytime = localtime(&now);
+
+      seconds = mytime->tm_sec;
+      minutes = mytime->tm_min;
+      hours = mytime->tm_hour;
+      
+      converttobin(hours);
+      std::cout<<"H:"<<converttobin(hours)<<" M:"<<converttobin(minutes)<<" S:"<<converttobin(seconds)<<"\r"<<std::flush;
+      sleep(1);
+    }
+ 
+  return 0;
+}
+
+string converttobin(int t)
+{
+  string bin;
+  do
+    {
+      if((t&1)==0)
+	{
+	  bin += "0";
+	}
+      else
+	{
+	  bin += "1";
+	}
+      t >>= 1;
+    } while (t);
+
+  reverse(bin.begin(), bin.end());
+  return bin;
+}
